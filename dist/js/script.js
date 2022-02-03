c36caefcbda3b6b77cef75bb39775fb883e0547e
satoshi kouno <luxeadd@gmail.com>
2022-02-03 17:57:37 +0900

    02031757

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,39 @@
+"use strict"
+
+
+//header
+//ハンバーガーメニュー---------------
+const jsHamburger = document.getElementById('js-hamburger');
+const body = document.body;
+const spHeaderMenu = document.getElementById('js-drawer-menu')
+const drawerBackground = document.getElementById('js-header__overlay')
+const drawerMenuItem = document.getElementById('js-drawer-menu__item')
+
+//ハンバーガーメニュークリックアクション
+jsHamburger.addEventListener('click', function() {
+  body.classList.toggle('is-drawerActive')
+  if (this.getAttribute('aria-expanded') == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+    spHeaderMenu.setAttribute('area-hidden','false')
+  } else {
+    this.setAttribute('aria-expanded', 'false')
+    spHeaderMenu.setAttribute('area-hidden','true')
+  };
+});
+//ドラワーメニュー展開時背景クリックアクション
+drawerBackground.addEventListener('click', () => {
+  body.classList.remove('is-drawerActive')
+  jsHamburger.setAttribute('aria-expanded', 'false')
+  spHeaderMenu.setAttribute('area-hidden','true')
+});
+//ドラワーメニュー展開時リストクリックアクション
+drawerMenuItem.addEventListener('click', () => {
+  body.classList.remove('is-drawerActive')
+  jsHamburger.setAttribute('aria-expanded', 'false')
+  spHeaderMenu.setAttribute('area-hidden','true')
+});
+
+
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
