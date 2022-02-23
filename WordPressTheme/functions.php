be5f70891192466994dcd22706baf8972a31c22a
satoshi kouno <luxeadd@gmail.com>
2022-02-23 20:33:20 +0900

    02232033

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -31,17 +31,21 @@ add_action( 'after_setup_theme', 'my_setup' );
  *
  * @codex https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
  */
+
+
+ 
+
+
+
 function my_script_init()
-{
-	wp_enqueue_style( 'google-webfont-style', '//fonts.googleapis.com/css?family=Fira+Sans:ital,wght@1,500&family=Noto+Sans+JP:wght@500;700&display=swap' );
-
+{	
 	wp_enqueue_style( 'my', get_template_directory_uri() . '/assets/css/styles.css', array(), '1.0.1', 'all' );
-
+	
 	if (is_front_page()){
 		wp_enqueue_script( 'my', get_template_directory_uri() . '/assets/js/top-script.js', array(  ), '1.0.1', true );
 	} else {
 		wp_enqueue_script( 'my', get_template_directory_uri() . '/assets/js/sub-script.js', array(  ), '1.0.1', true );
-}
+	}
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
